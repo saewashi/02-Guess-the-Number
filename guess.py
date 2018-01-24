377bc58141b779a0a7e7a5b751fbf5b499727246
saewashi <35227909+saewashi@users.noreply.github.com>
2018-01-24 15:00:35 -0500

    Guess the Number
    Completes assigned task of the assignment, which is to create a game that asks the user to guess the number, which will be randomized between a range of numbers.

--- guess.py
+++ guess.py
@@ -4,26 +4,61 @@ import sys, random
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-#variables
-guesses = 10
-guess_range = 20
+#Variables
+guess = 0
+guesses = 0
+
+# Intro to the game, and welcoming the player.
+print("Welcome to Guess a Number!")
 
 
-#generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
-number = random.randint(1,guess_range)
 
-#ask the user for a response and store it in the variable [guess]
-guess = input()
+# Random ranges for the user to guess between
+number = random.randrange(1, 11)
 
 
-#a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
-try:
-	#convert the guess to an integer
-	guess = int(guess)
+# Assigning guess to be converted into int, and asking for the users input
 
-	#check if the guess is less than the random number
-	if guess < number:
-		print('Too low!')
+while guess != number:
+    guess = int(input("Please Guess a number: "))
+    guesses = guesses + 1
 
-except ValueError:
-	print('Please enter a whole number.')
+
+# Sees if the user's guesses are too low, too high, or the correct answer.
+
+    if guess < number:
+        print('Too Low! Guess again.')
+    elif guess > number:
+        print("Too High! Guess again.")
+    else:
+        print("You got it! Great job!")
+
+
+
+#Prints the number of guess/tries it took the user to guess.
+print("Number of guesses:", guesses)
+
+# Bonus Material
+# Asks user for the bonus question
+print("Bonus Question: What's my age?")
+
+
+# Set age number. . .Just pretend not to see that. :)
+number = 20
+
+
+# Assigning guess to be converted into int, and asking for the users input
+
+while guess != number:
+    guess = int(input("Please Guess my age: "))
+    guesses = guesses + 1
+
+
+# Sees if the user's guesses are too low, too high, or the correct answer.
+
+    if guess < number:
+        print('Nope! Too low. Guess again.')
+    elif guess > number:
+        print("Nope! Too high Guess again.")
+    else:
+        print("You got my age! Great job!")
